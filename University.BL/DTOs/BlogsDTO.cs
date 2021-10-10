b3b330054e8f60197bac94048c7f7902739b57b3
Steven'sWorks <63211283+Smartinezpelaez@users.noreply.github.com>
2021-10-09 23:11:10 -0500

    BlogsDto

---
+++ University.BL/DTOs/BlogsDTO.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.BL.DTOs
+{
+    public class BlogsDTO
+    {
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("BloggerName")]
+        public string BloggerName { get; set; }
+
+        [JsonProperty("Posts")]
+        public string Posts { get; set; }
+
+    }
+
+        public class PostDTO
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("DateCreate")]
+            public DateTime DateCreate { get; set; }
+
+            [JsonProperty("Content")]
+            public string Content { get; set; }
+
+        }
+    
+}
