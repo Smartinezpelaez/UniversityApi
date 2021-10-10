0b1b4f41f6f51a9eb8341ad6ebeded53435fd66c
Steven'sWorks <63211283+Smartinezpelaez@users.noreply.github.com>
2021-10-09 23:08:56 -0500

    Se crea BlogsDTO

---
+++ University.Web/Controllers/InstructorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using University.BL.DTOs;
+using University.BL.Services.Implements;
+
+
+namespace University.Web.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly ApiService apiService = new ApiService();
+        public async Task<IActionResult> Index()
+        {
+            var instructorDTO = await apiService.RequestAPI<List<InstructorOutputDTO>>(BL.Helpers.Endpoints.URL_BASE,
+                "api/Instructor/GetAll/",
+                null,
+                ApiService.Method.Get);
+
+            var instructor = (List<InstructorOutputDTO>)instructorDTO.Data;
+            return View(instructor);
+        }
+    }
+}
