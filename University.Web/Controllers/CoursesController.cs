4303f1980669e5ec14535384ffb2ae23b2faa7c1
Steven'sWorks <63211283+Smartinezpelaez@users.noreply.github.com>
2021-10-13 12:01:49 -0500

    Se crea el proyecto universityFunction con su respectivo dto y servicios.

--- University.Web/Controllers/CoursesController.cs
+++ University.Web/Controllers/CoursesController.cs
@@ -21,5 +21,81 @@ namespace University.Web.Controllers
             var courses = (List<CourseOutputDTO>)responseDTO.Data;
             return View(courses);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CourseDTO());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CourseDTO courseDTO)
+        {
+            var responseDTO = await apiService.RequestAPI<CourseDTO>(BL.Helpers.Endpoints.URL_BASE,
+                "api/Courses/",
+                courseDTO,
+                ApiService.Method.Post);
+
+            if (responseDTO.Code == (int)HttpStatusCode.OK)
+                return RedirectToAction(nameof(Index));
+
+            return View(courseDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var responseDTO = await apiService.RequestAPI<CourseOutputDTO>(BL.Helpers.Endpoints.URL_BASE,
+              "api/Courses/GetById/" + id,
+              null,
+              ApiService.Method.Get);
+
+            var course = (CourseOutputDTO)responseDTO.Data;
+
+            return View(course);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CourseOutputDTO courseDTO)
+        {
+            var responseDTO = await apiService.RequestAPI<CourseDTO>(BL.Helpers.Endpoints.URL_BASE,
+                "api/Courses/" + courseDTO.CourseID,
+                courseDTO,
+                ApiService.Method.Put);
+
+            if (responseDTO.Code == (int)HttpStatusCode.OK)
+                return RedirectToAction(nameof(Index));
+
+            return View(courseDTO);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var responseDTO = await apiService.RequestAPI<CourseOutputDTO>(BL.Helpers.Endpoints.URL_BASE,
+              "api/Courses/" + id,
+              null,
+              ApiService.Method.Get);
+
+            var course = (CourseOutputDTO)responseDTO.Data;
+
+            return View(course);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(CourseOutputDTO courseDTO)
+        {
+            var responseDTO = await apiService.RequestAPI<CourseOutputDTO>(BL.Helpers.Endpoints.URL_BASE,
+              "api/Courses/" + courseDTO.CourseID,
+              null,
+              ApiService.Method.Delete);
+
+            if (responseDTO.Code == (int)HttpStatusCode.OK)
+                return RedirectToAction(nameof(Index));
+
+            return View(courseDTO);
+        }
+
+
     }
 }
